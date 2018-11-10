70465791e273ab21cc22cba6c1bc928c2d432e2c
Sebastian Aburto <seba.aburto@gmail.com>
2018-11-10 14:19:36 -0300

    Add check of null

--- src/main/java/com/saburto/ruts/Rut.java
+++ src/main/java/com/saburto/ruts/Rut.java
@@ -1,5 +1,7 @@
 package com.saburto.ruts;
 
+import java.util.Objects;
+
 /**
  * TODO Add javadoc
  */
@@ -7,7 +9,7 @@ public class Rut {
   private final int number;
   private final String checkDigit;
 
-  public Rut(int number, String checkDigit) {
+  private Rut(int number, String checkDigit) {
     this.number = number;
     this.checkDigit = checkDigit;
   }
@@ -21,6 +23,10 @@ public class Rut {
   }
 
   public static Rut parse(String rawRut) {
+    Objects.requireNonNull(rawRut, "raw rut must not be null");
+    if (!rawRut.matches("[\\d]{1,10}-[\\dkK]{1}")) {
+      throw new IllegalArgumentException(String.format("Illegal formant from %s", rawRut));
+    }
 
     return new Rut(1, "9");
   }
--- src/test/java/com/saburto/ruts/TestRut.java
+++ src/test/java/com/saburto/ruts/TestRut.java
@@ -11,4 +11,28 @@ public class TestRut {
     assertThat(rut.getNumber()).isEqualTo(1);
     assertThat(rut.getCheckDigit()).isEqualTo("9");
   }
+
+  @Test
+  void parseNull() {
+    assertThatExceptionOfType(NullPointerException.class)
+      .isThrownBy( () -> Rut.parse(null))
+      .withMessageContaining("rut")
+      .withNoCause();
+  }
+
+  @Test
+  void parseInvalidString() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+      .isThrownBy( () -> Rut.parse("helloWorld"))
+      .withMessageContaining("helloWorld")
+      .withNoCause();
+  }
+
+  @Test
+  void parseInvalidCheckDigit() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+      .isThrownBy( () -> Rut.parse("1-q"))
+      .withMessageContaining("1-q")
+      .withNoCause();
+  }
 }
