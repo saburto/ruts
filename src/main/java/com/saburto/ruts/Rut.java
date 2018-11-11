8308bbe2ac88fc2107ab0786664c52eab34d7c64
Sebastian Aburto <seba.aburto@gmail.com>
2018-11-11 09:18:55 -0300

    Add format validtion

--- src/main/java/com/saburto/ruts/Rut.java
+++ src/main/java/com/saburto/ruts/Rut.java
@@ -1,11 +1,24 @@
 package com.saburto.ruts;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * TODO Add javadoc
  */
 public class Rut {
+
+  private static final String PATTERN =
+    "^(?<number>[\\d]{1,3}(\\.[\\d]{3})*|\\d+)" + // Number part
+    "-" + // Separator
+    "(?<check>[\\dkK]{1})$"; // Check digit
+
+  private static Pattern FORMAT_PATTERN = Pattern.compile(PATTERN);
+
   private final int number;
   private final String checkDigit;
 
@@ -24,10 +37,27 @@ public class Rut {
 
   public static Rut parse(String rawRut) {
     Objects.requireNonNull(rawRut, "raw rut must not be null");
-    if (!rawRut.matches("[\\d]{1,10}-[\\dkK]{1}")) {
-      throw new IllegalArgumentException(String.format("Illegal formant from %s", rawRut));
+
+    Matcher rutMatcher = FORMAT_PATTERN.matcher(rawRut);
+    if (!rutMatcher.matches()) {
+      throwInvalidExeption(rawRut);
     }
 
-    return new Rut(1, "9");
+    String numberString = rutMatcher.group("number");
+    String checkDigit = rutMatcher.group("check");
+
+    Number v = null;
+    try {
+      v = NumberFormat.getNumberInstance(new Locale("es", "CL"))
+        .parse(numberString);
+    } catch (ParseException e) {
+      throwInvalidExeption(rawRut);
+    }
+
+    return new Rut(v.intValue(), checkDigit);
+  }
+
+  private static void throwInvalidExeption(String rawRut) {
+      throw new IllegalArgumentException(String.format("Illegal format of [%s]", rawRut));
   }
 }
--- src/test/java/com/saburto/ruts/TestRut.java
+++ src/test/java/com/saburto/ruts/TestRut.java
@@ -13,6 +13,27 @@ public class TestRut {
   }
 
   @Test
+  void parseSimpleRutWithK() {
+    Rut rut = Rut.parse("1-k");
+    assertThat(rut.getNumber()).isEqualTo(1);
+    assertThat(rut.getCheckDigit()).isEqualTo("k");
+  }
+
+  @Test
+  void parseLargeRut() {
+    Rut rut = Rut.parse("1100001000-9");
+    assertThat(rut.getNumber()).isEqualTo(1_100_001_000);
+    assertThat(rut.getCheckDigit()).isEqualTo("9");
+  }
+
+  @Test
+  void parseLargeRutWhitThousandSeparators() {
+    Rut rut = Rut.parse("1.100.001.000-9");
+    assertThat(rut.getNumber()).isEqualTo(1_100_001_000);
+    assertThat(rut.getCheckDigit()).isEqualTo("9");
+  }
+
+  @Test
   void parseNull() {
     assertThatExceptionOfType(NullPointerException.class)
       .isThrownBy( () -> Rut.parse(null))
